f5ab9ff36f812a4db9e0345aebccf074cf9e530d
olili27 <timmolili27@gmail.com>
2025-04-02 00:19:58 +0300

    add now code to pass the newly added testcase

---
+++ fibonacci.js
@@ -0,0 +1,7 @@
+/**
+ * @param {number} number - A positive integer to compute a fibonacci of
+ * @returns {number} The fibonacci of the given number
+ */
+export default function fibonacci(number) {
+  return 0;
+}
