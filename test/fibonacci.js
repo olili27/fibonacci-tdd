8d659023bdb6139478f5f87c211e0a8acc9d774d
olili27 <timmolili27@gmail.com>
2025-04-02 00:19:25 +0300

    add testcase fibonacci(0)

---
+++ test/fibonacci.js
@@ -0,0 +1,18 @@
+/* eslint-disable import/extensions */
+/* eslint-disable no-undef */
+/* eslint-disable func-names */
+/* eslint-disable prefer-arrow-callback */
+
+// timothy olili
+// bridget nagasha
+
+import { assert } from "chai";
+import fibonacci from "../fibonacci.js";
+
+describe("Fibonacci Function for Positive Integers", function () {
+  describe("Zero and Positive Integers", function () {
+    it("fibonacci(0) should return 0", function () {
+      assert.strictEqual(fibonacci(0), 0);
+    });
+  });
+});
